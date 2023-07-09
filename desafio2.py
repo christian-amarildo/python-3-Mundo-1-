3f1d973fca51467bc58bdef28f9ee70cb477e24e
christian-amarildo <127919702+christian-amarildo@users.noreply.github.com>
2023-07-08 23:48:05 -0300

    Add files via upload

---
+++ desafio2.py
@@ -0,0 +1,5 @@
+dia= input("digite o dia do seu nascimento: ")
+mes= input("digite o mês do seu nascimento: ")
+ano= input("digite o ano do seu nascimento: ")
+
+print("você nasceu no dia",dia,"de", mes, "de.",ano,"Correto?")
