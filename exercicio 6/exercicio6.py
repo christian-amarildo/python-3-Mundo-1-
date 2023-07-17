f7d1a3e5f231db753025c9487400e5776eceeecc
christian-amarildo <127919702+christian-amarildo@users.noreply.github.com>
2023-07-17 16:25:39 -0300

    Add files via upload

---
+++ exercicio 6/exercicio6.py
@@ -0,0 +1,8 @@
+numero= int(input("insira um numero: "))
+dobro= numero * 2 
+triplo =  numero * 3
+raiz = numero ** (1/2)
+print("numero: ",numero)
+print("dobro: ",dobro)
+print("triplo: ",triplo)
+print("raiz: ",raiz)
