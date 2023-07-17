e5c530333d0c51bdaba965f82a0d0deed014d01d
christian-amarildo <127919702+christian-amarildo@users.noreply.github.com>
2023-07-17 16:22:28 -0300

    Add files via upload

---
+++ exercicio 5/exercicio5.py
@@ -0,0 +1,4 @@
+numero= int(input("insira um número"))
+antecessor = numero - 1
+sucessor = numero + 1
+print("o número {} tem como sucessor = {} e como antecessor = {}".format(numero,sucessor,antecessor))
