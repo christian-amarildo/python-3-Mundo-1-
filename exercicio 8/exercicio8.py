15b774dbcc44964c69f80b1c136b6ee56acc4cb8
christian-amarildo <127919702+christian-amarildo@users.noreply.github.com>
2023-07-18 03:56:15 -0300

    Add files via upload

---
+++ exercicio 8/exercicio8.py
@@ -0,0 +1,4 @@
+metro = int(input("insira o valor em metro"))
+centimetro = metro * 100
+milimetro = metro * 1000
+print("o valor em metro", metro,"é equivalente a ", centimetro,"centimetros e",milimetro,"milimetros")
